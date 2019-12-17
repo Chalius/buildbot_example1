bb3af8cf9af2aa85ea18a2d8c0e35f6ea1dc9d31
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-17 12:04:17 -0500

    7

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ objeto = Principal;
 class TestSum(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(objeto.suma(5, 3), 8, "Should be 26")
+        self.assertEqual(objeto.suma(5, 5), 8, "Should be 26")
 
 
 if __name__ == '__main__':
