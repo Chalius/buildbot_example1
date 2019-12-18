f6abb79b80a509fbea7d3646c30bfb736a94ebba
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-18 08:46:11 -0500

    16

--- test1.py
+++ test1.py
@@ -2,12 +2,12 @@ from archivo1 import Principal
 import unittest
 
 objeto = Principal();
-
+print("mi suma: ", objeto.suma(5, 4))
 
 class TestSum(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(objeto.suma(5, 4), 9, "Should be 26")
+        self.assertEqual(objeto.suma(5, 4), 9, "Should be 9")
 
 if __name__ == '__main__':
     unittest.main()
