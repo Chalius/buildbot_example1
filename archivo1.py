8224ccfda58266a370c6cb222859fdc700954d98
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-19 13:28:38 -0500

    37

--- archivo1.py
+++ archivo1.py
@@ -3,12 +3,8 @@
 class Principal:
 
     def suma(self, num1, num2):
-        print("forzando conflitos en merge")
-
-        num3 = num1
-        print("forzando conflitos en merge")
-
-        return (num1 + num2)
+        solucion = num1 + num2
+        return (solucion + num2)
 
 # git push https://
 #
