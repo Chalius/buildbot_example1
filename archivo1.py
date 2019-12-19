16456a85ae30b0341097cb3714518264603425a0
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-19 11:56:18 -0500

    modificación 1 desde pruebaTesting2

--- archivo1.py
+++ archivo1.py
@@ -1,6 +1,6 @@
 class Principal:
 
     def suma(self, num1, num2):
-        return (num1 + num2)
+        return (num1 + num2+12)
 
 #git push https://github.com/Chalius/buildbot_example1.git deploy --force
