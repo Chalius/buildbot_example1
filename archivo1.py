e27098cde81b65ead2cfe6379313fb0256799a77
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-19 12:00:47 -0500

    28

--- archivo1.py
+++ archivo1.py
@@ -1,7 +1,7 @@
 class Principal:
 
     def suma(self, num1, num2):
-        return (num1 + num2+12)
+        return (num1 + num2 + 15)
 
 
 #git push https://github.com/Chalius/buildbot_example1.git deploy --force
