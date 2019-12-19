320159fd0e25aed933b565ee8bac632c8ae87ae0
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-19 13:24:37 -0500

    36

--- archivo1.py
+++ archivo1.py
@@ -1,7 +1,17 @@
+
+
 class Principal:
 
     def suma(self, num1, num2):
+        print("forzando conflitos en merge")
+
         num3 = num1
+        print("forzando conflitos en merge")
+
         return (num1 + num2)
 
-#git push https://github.com/Chalius/buildbot_example1.git deploy --force
+# git push https://
+#
+# github.com/Chali
+#
+# us/buildbot_example1.git deploy --force
