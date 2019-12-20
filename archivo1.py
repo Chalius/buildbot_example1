d482ae111c07b16b694262cab30d81d43235a52d
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-20 09:25:20 -0500

    41

--- archivo1.py
+++ archivo1.py
@@ -5,6 +5,7 @@ class Principal:
     def suma(self, num1, num2):
         print("probando try")
         print("probando try2")
+        num3 = num1 + num2 / 2
         return (num1 + num2)
 
 # git push https://github.com/Chalius/buildbot_example1.git deploy --force
