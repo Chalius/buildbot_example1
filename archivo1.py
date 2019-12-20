63b791734b2e52d87d9b978769e03344689e2e15
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-20 12:06:26 -0500

    46

--- archivo1.py
+++ archivo1.py
@@ -5,4 +5,5 @@ class Principal:
     # git push https://github.com/Chalius/buildbot_example1.git deploy --force
 
     def suma(self, num1, num2):
+        print("wait y quiet juntos funcionaron perfectamente!")
         return (num1 + num2)
