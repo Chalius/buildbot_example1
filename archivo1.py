1ebd2752bba51f2aee66aa55695f8fe6a2490d45
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-20 11:31:10 -0500

    45

--- archivo1.py
+++ archivo1.py
@@ -2,6 +2,7 @@
 
 class Principal:
 
+    # git push https://github.com/Chalius/buildbot_example1.git deploy --force
+
     def suma(self, num1, num2):
-        print("trytry2")
         return (num1 + num2)
