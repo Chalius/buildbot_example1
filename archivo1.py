74b2c8236a2b1d75ff7e59f3171645f7b4d10733
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-27 08:37:02 -0500

    51

--- archivo1.py
+++ archivo1.py
@@ -3,6 +3,6 @@ class Principal:
     # git push https://github.com/Chalius/buildbot_example1.git deploy --force
     def suma(self, num1, num2):
         print("24/12/2019")
-        print("feliz navidad")
+        print("feliz navidad2")
         print("probando schedulers dependent")
         return (num1 + num2)
