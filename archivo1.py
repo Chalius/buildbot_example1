a2942e7b42fbfa60a7fd241393bfc8e89fdc7b91
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-12-31 09:36:26 -0500

    55

--- archivo1.py
+++ archivo1.py
@@ -3,6 +3,7 @@ class Principal:
     def suma(self, num1, num2):
         print("24/12/2019")
         print("feliz navidad5")
+        print("feliz año nuevo")
         print("probando schedulers dependent")
 
         return (num1 + num2)
