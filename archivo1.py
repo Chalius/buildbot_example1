a3fb1800c95bb9445d0394394c1816f2cd3b847b
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2020-01-02 12:46:02 -0500

    60

--- archivo1.py
+++ archivo1.py
@@ -5,5 +5,6 @@ class Principal:
         print("feliz navidad5")
         print("feliz año nuevo5")
         print("probando schedulers dependent")
-
+        print("ya estamos 2020")
+        print("probando method fresh")
         return (num1 + num2)
